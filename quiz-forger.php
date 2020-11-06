b1b66b478c4f6b6ebf1809e3a345cf177105422d
iths-anna-boberg <amboberg@gmail.com>
2020-11-06 09:03:34 +0100

    plugin meta

---
+++ quiz-forger.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Plugin Name: Quiz Forger
+* Plugin URI: https://iths-bananbyran.github.io/banana-agency-website/
+* Description: Create quizes.
+* Version: 1.0
+* Author: Bananbyrån
+*/
