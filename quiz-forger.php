82d024a30b4bc48187d7eb8e613facb60e41eddd
iths-anna-boberg <amboberg@gmail.com>
2020-11-08 22:20:45 +0100

    comments

--- quiz-forger.php
+++ quiz-forger.php
@@ -7,9 +7,20 @@
 * Author: Bananbyrån
 */
 
+//Aktivera pluginet - sätta upp tabeller
 function activateQuizForger() {
     require_once plugin_dir_path( __FILE__ ) . 'includes/class-quiz-forger-activator.php';
     Quiz_Forger_Activator::do_activate();
 }
+
+//Skapa adminvy för skapa frågor
+
+//skapa adminvy för skapa quiz, ska kunna hämta skapade frågor
+
+//skapa adminvy för alla befintliga quiz så man kan se dem och deras shortcode där id ingår
+
+//skapa shortcode
+
+//skapa api endpoints
 
 register_activation_hook( __FILE__, 'activateQuizForger' );
