13ab654775a5ffd2a5f412d95faadbf7f2145dd3
iths-anna-boberg <amboberg@gmail.com>
2020-11-15 08:56:07 +0100

    add questions function

--- includes/class-admin-quiz-forger.php
+++ includes/class-admin-quiz-forger.php
@@ -56,7 +56,7 @@ class Admin_Quiz_Forger {
         echo "<form action='admin-post.php' method='post' class='qf-form'>";
         wp_nonce_field( 'create_quiz_' . $event );
         echo '<input type="hidden" name="action" value="create_quiz">';
-        echo "<input type='hidden' name='eventid' value=$event>";
+        echo "<input type='hidden' name='eventid' value='$event'>";
         echo '<label>Name your quiz: </label><input type:"text" name="quizName">';
         echo '<label>Describe your quiz: </label><textarea rows="5" cols="75" name="description"></textarea>';
         echo '<input type="submit" value="Submit" class="qf-btn-admin">';
@@ -89,7 +89,69 @@ class Admin_Quiz_Forger {
 
     public static function add_questions() {
 
-        echo '<h2 class="qf-heading">Create Questions</h2>';
+        echo '<div class="qf-wrap">
+        <h2 class="qf-heading">Create the questions for your quiz!</h2>
+    
+        <div class="qf-form-wrapper">
+          <form action="admin-post.php" method="post" id="qf-questionmaker-form" >
+          <input type="hidden" name="action" value="submit_question">
+            
+            <label for="qf-quiz-select-input">Select your quiz</label>';
+
+        echo '<select id="qf-quiz-select-input" form="qf-questionmaker-form" name="quiz-list" class="qf-text-input">
+              <option selected disabled>Select a Quiz</option>';
+        
+        self::render_quiz_select();
+
+        echo '</select>';
+        echo '<div class="qf-input-wrapper">
+                <label for="qf-question-input">Write your question here</label>
+                <input type="text" id="qf-question-input" class="qf-text-input" name="question" >
+            </div>
+
+            <div class="qf-input-wrapper">
+                <label for="qf-question-input">Answer 1: </label>
+                <input type="text" id="qf-answer1-input" class="qf-text-input" name="answer1" required>
+                <input type="radio" name="right_answer" id="answer1"  checked  value="1">
+                <label for="answer1">Correct</label>
+            </div>
+
+            <div class="qf-input-wrapper">
+                <label for="qf-question-input">Answer 2: </label>
+                <input type="text" id="qf-answer2-input" class="qf-text-input" name="answer2" required>
+                <input type="radio" name="right_answer" id="answer2" value="2">
+                <label for="answer2">Correct</label>
+            </div>
+
+            <div class="qf-input-wrapper">
+                <label for="qf-question-input">Answer 3: </label>
+                <input type="text" id="qf-answer3-input" class="qf-text-input" name="answer3" required>
+                <input type="radio" name="right_answer" id="answer3" value="3">
+                <label for="answer3">Correct</label>
+            </div>
+
+            <div class="qf-input-wrapper">
+                <label for="qf-question-input">Answer 4: </label>
+                <input type="text" id="qf-answer4-input" class="qf-text-input" name="answer4" required>
+                <input type="radio" name="right_answer" id="answer4" value="4">
+                <label for="answer4">Correct</label>
+            </div>
+            <label for="qf-explanation">Add explanation to the answer? (Optional)</label>
+            <textarea name="qf-explanation" id="qf-explanation" style="overflow:auto;resize:none" cols="30" rows="10"></textarea>
+            
+            <input type="submit" value="Add question" name="submit">
+                </form>
+                    </div>
+                        </div>';
+    }
+
+    public static function render_quiz_select(){
+
+        global $wpdb;
+        $quizes_table = $wpdb->prefix . 'quizforgequizes';
+        $quiz_query = $wpdb->get_results( "SELECT * FROM $quizes_table" );
+        foreach ($quiz_query as $quiz_data) { echo "<option value='" . $quiz_data->quiz_id . "'>" . $quiz_data->title . "</option>"; }
+
     }
 
     public static function quiz_dashboard_menu() {
