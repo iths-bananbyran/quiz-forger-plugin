e856a8610d478e5254d30fa26c2f1afd6cd8ac7c
olle-forsslof <54362382+olle-forsslof@users.noreply.github.com>
2020-12-16 15:42:05 +0100

    Created API endpoints for react frontend

--- quiz-forger.php
+++ quiz-forger.php
@@ -37,6 +37,68 @@ function quiz_shortcode($atts, $content = null) {
     return ob_get_clean();
 }
 
+// ENDPOINTS
+
+// Get any number of posts/quizzes 
+function get_num_of_posts($num_of_posts) {
+    if ($num_of_posts['numberposts'] == "all" ) {
+        $integer = -1;
+    } else {
+        $integer = intval($num_of_posts['numberposts']);
+    };
+    
+    $args = array( 
+        'post_content',
+        'post_status' => 'publish',
+        'post_title',
+        'numberposts' => $integer,
+        'post_type' => 'post' );
+
+    $posts_arr = get_posts($args);
+
+    $posts_info = array();
+
+    forEach( $posts_arr as $post ){
+        $thumb_url = get_the_post_thumbnail_url($post -> ID);
+        $category = get_the_category($post -> ID);
+        $result = array();
+        $result["quiz_id"] = filter_quiz_id( $post -> post_content );
+        $result["quiz_title"] = $post -> post_title;
+        $result["quiz_thumbnail"] = $thumb_url;
+        $result["quiz_category"] = $category;
+        array_push( $posts_info, $result );
+    }
+
+    return $posts_info;
+}
+
+function filter_quiz_id($string) {
+    preg_match("/=\d*]/", $string, $matches);
+    $quiz_id = substr($matches[0], 1, -1);
+    return $quiz_id;
+}
+
+add_action( 'rest_api_init', function() {
+    register_rest_route( 'quizforge/v1', '/quizposts/(?P<numberposts>\S+)', array(
+      'methods' => 'GET',
+      'callback' => 'get_num_of_posts',
+    ) );
+} );
+
+// Get specific quiz based on ID
+
+function get_quiz($id) {
+    require_once plugin_dir_path( __FILE__ ) . 'public/class-render-quiz.php';
+    return Render::quiz_object($id['id']);
+}
+
+add_action( 'rest_api_init', function() {
+    register_rest_route( 'quizforge/v1', '/quiz/(?P<id>\d+)', array(
+      'methods' => 'GET',
+      'callback' => 'get_quiz',
+    ) );
+} );
+
 register_activation_hook( __FILE__, 'activate_quiz_forger' );
 add_action('admin_menu', array($quiz_forge_admin, 'quiz_dashboard_menu'));
 add_action('admin_enqueue_scripts', array($quiz_forge_admin, 'qf_admin_style'));
